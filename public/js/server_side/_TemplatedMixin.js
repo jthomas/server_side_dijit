4aa16e64e2df6312de2324cb111df7a8fa8a7b97
James Thomas <james.thomas@uk.ibm.com>
2012-07-04 22:17:55 +0100

    Fixing issue with classes having custom "buildRendering" methods.
    We now monkey-patch method in _TemplatedMixin rather than overriding
    directly. Use safety flag in mixin class to ensure we are actually
    being called for a server-rendered widget, otherwise we delegate back
    to the old method.
    
    This allows us to maintain the correct inheritance chaining, rather than
    interfering.

--- public/js/server_side/_TemplatedMixin.js
+++ public/js/server_side/_TemplatedMixin.js
@@ -1,58 +1,37 @@
-define(["dojo/_base/declare", "dijit/_WidgetBase"], function (declare, _WidgetBase) {
+define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin"], function (declare, _WidgetBase, _TemplatedMixin) {
+
+    var old = _TemplatedMixin.prototype.buildRendering;
+        
+    _TemplatedMixin.prototype.buildRendering = function () {
+        if (!this.serverSide) {
+            return old.call(this);
+        }
+
+        var node = this.srcNodeRef;
+
+        node.removeAttribute("data-dojo-type");
+
+        // Call down to _Widget.buildRendering() to get base classes assigned
+        // TODO: change the baseClass assignment to _setBaseClassAttr
+        //	this.inherited(arguments);
+        _WidgetBase.prototype.buildRendering.call(this);
+
+        // FIX ME: We don't want to descend into inner template widgets.
+        // recurse through the node, looking for, and attaching to, our
+        // attachment points and events, which should be defined on the template node.
+        this._attachTemplateNodes(node, function(n,p){ return n.getAttribute(p); });
+
+        this._beforeFillContent();		// hook for _WidgetsInTemplateMixin
+    };
+
+
+
     return declare(null, {
 
+        serverSide: true,
+
         stopParser: true,
-
-        buildRendering: function(){
-            //
-            //
-			// summary:
-			//		Construct the UI for this widget from a template, setting this.domNode.
-			// tags:
-			//		protected
-
-/*			if(!this.templateString){
-				this.templateString = cache(this.templatePath, {sanitize: true});
-			}
-
-			// Lookup cached version of template, and download to cache if it
-			// isn't there already.  Returns either a DomNode or a string, depending on
-			// whether or not the template contains ${foo} replacement parameters.
-			var cached = _TemplatedMixin.getCachedTemplate(this.templateString, this._skipNodeCache, this.ownerDocument);
-
-			var node;
-			if(lang.isString(cached)){
-				node = domConstruct.toDom(this._stringRepl(cached), this.ownerDocument);
-				if(node.nodeType != 1){
-					// Flag common problems such as templates with multiple top level nodes (nodeType == 11)
-					throw new Error("Invalid template: " + cached);
-				}
-			}else{
-				// if it's a node, all we have to do is clone it
-				node = cached.cloneNode(true);
-			}
-            */
-			
-            var node = this.srcNodeRef;
-
-            node.removeAttribute("data-dojo-type");
-
-			// Call down to _Widget.buildRendering() to get base classes assigned
-			// TODO: change the baseClass assignment to _setBaseClassAttr
-		//	this.inherited(arguments);
-            _WidgetBase.prototype.buildRendering.call(this);
-
-            // FIX ME: We don't want to descend into inner template widgets.
-			// recurse through the node, looking for, and attaching to, our
-			// attachment points and events, which should be defined on the template node.
-			this._attachTemplateNodes(node, function(n,p){ return n.getAttribute(p); });
-
-			this._beforeFillContent();		// hook for _WidgetsInTemplateMixin
-
-            // Nodes already have been copied in rendered template... don't need to do anything else.
-	        // 		this._fillContent(this.srcNodeRef);
-		},
-
+        
         // Ensure this function doesn't descend into container nodes of the rendered
         // templates of child widgets. With client-side rendering all the child nodes from
         // the root would be traversed as child widgets haven't been instantiated. In our case,
